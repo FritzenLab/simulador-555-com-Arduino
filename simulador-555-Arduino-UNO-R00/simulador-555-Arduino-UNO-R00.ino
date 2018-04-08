e35899f4f54e88728d2e5693d01ea80b96bdc9bc
Clovis <clovisf@gmail.com>
2018-04-08 19:31:29 -0300

    primeiro upload, sketch

---
+++ simulador-555-Arduino-UNO-R00/simulador-555-Arduino-UNO-R00.ino
@@ -0,0 +1,16 @@
+#define 2_trigger A0
+#define 5_control A1
+#define 6_threshold A2
+
+
+void setup() {
+  pinMode(3_output, OUTPUT);
+  pinMode(4_reset, INPUT);
+  pinMode(7_discharge, OUTPUT);
+  
+}
+
+void loop() {
+ 
+
+}
