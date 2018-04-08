d1c5df1900ae96ea841a5232debea97b6f280e3e
Clovis <clovisf@gmail.com>
2018-04-08 19:52:22 -0300

    mais codigo do 555

--- simulador-555-Arduino-UNO-R00/simulador-555-Arduino-UNO-R00.ino
+++ simulador-555-Arduino-UNO-R00/simulador-555-Arduino-UNO-R00.ino
@@ -1,16 +1,28 @@
-#define 2_trigger A0
-#define 5_control A1
-#define 6_threshold A2
-
+#define trigger_2 A0
+#define control_5 A1
+#define threshold_6 A2
+#define output_3 2
+#define reset_4 3
+#define discharge_7 4
 
 void setup() {
-  pinMode(3_output, OUTPUT);
-  pinMode(4_reset, INPUT);
-  pinMode(7_discharge, OUTPUT);
+  
+  pinMode(output_3, OUTPUT);
+  pinMode(reset_4, INPUT);
+  pinMode(discharge_7, OUTPUT);
   
 }
 
 void loop() {
- 
+ if (trigger_2 <= 341){ // IF trigger pin voltage is smaller than 33% of the supply voltage
+  
+  digitalWrite(output_3, HIGH); // Output pin goes HIGH
+  digitalWrite(discharge_7, LOW); // Discharge pin goes OFF
+
+  
+ } else if ( threshold_6 >= 682){
+  digitalWrite(output_3, LOW); // Output pin goes HIGH
+  digitalWrite(discharge_7, HIGH); // Discharge pin goes OFF
+ }
 
 }
